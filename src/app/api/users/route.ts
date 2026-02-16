c04eee4f2f798b4885c6fe9cde89e113efa42079
Paul Lorber <76565967+paullorb@users.noreply.github.com>
2026-02-16 16:17:11 +0100

    add MongoDB integration and create API route for user authentication

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,12 @@
+import clientPromise from "@/app/lib/mongo"
+import { NextResponse } from "next/server"
+
+export async function GET(req: Request) {
+  const authHeader = req.headers.get("Authorization")
+  const token = authHeader?.split(" ")[1]
+
+  if (token !== process.env.MONGO_SECRET) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+  }
+  return NextResponse.json({ success: true })
+}
---
+++ src/app/lib/mongo.ts
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb"
+
+const uri = process.env.MONGODB_URI as string
+
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined
+}
+
+let client: MongoClient
+let clientPromise: Promise<MongoClient>
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri)
+  global._mongoClientPromise = client.connect()
+}
+
+clientPromise = global._mongoClientPromise
+
+export default clientPromise
